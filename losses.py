e2f30c784439e90eca3a07303bdbefc62b4109c8
Sayuri Modi <156031013+sayurimodi@users.noreply.github.com>
2026-05-06 21:22:47 -0400

    Implement ClassificationLoss for multiclass classification
    Added ClassificationLoss class for multiclass classification using CrossEntropyLoss.

--- losses.py
+++ losses.py
@@ -3,6 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 # regular
+# using multiclass classification for the SEED-IV (4 classes) or SEED (3 classes i think) dataset
+class ClassificationLoss(nn.Module):
+   def __init__(self):
+        super().__init__()
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, logits, labels):
+        return self.loss_fn(logits, labels)
 
 
 # contrastive
